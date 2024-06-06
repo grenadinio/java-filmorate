798736c03905c246c2488396970df1360138f422
grenadinio <grenadinio@yandex.ru>
2024-06-06 17:46:42 +0300

    Добавление базы данных и базового заполнения

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+merge into MPA_RATING (ID, NAME)
+    values (1, 'G');
+merge into MPA_RATING (ID, NAME)
+    values (2, 'PG');
+merge into MPA_RATING (ID, NAME)
+    values (3, 'PG-13');
+merge into MPA_RATING (ID, NAME)
+    values (4, 'R');
+merge into MPA_RATING (ID, NAME)
+    values (5, 'NC-17');
+
+merge into GENRES (ID, NAME)
+    values (1, 'Комедия');
+merge into GENRES (ID, NAME)
+    values (2, 'Драма');
+merge into GENRES (ID, NAME)
+    values (3, 'Мультфильм');
+merge into GENRES (ID, NAME)
+    values (4, 'Триллер');
+merge into GENRES (ID, NAME)
+    values (5, 'Документальный');
+merge into GENRES (ID, NAME)
+    values (6, 'Боевик');
